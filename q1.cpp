7b892b7682f9f3a1027355fa4abdefb43f90f095
Charusingh-cs <charusandhu272@gnail.com>
2026-09-01 16:20:25 +0530

    Questin1

--- q1.cpp
+++ q1.cpp
@@ -1,1 +1,28 @@
-//
+#include <iostream>
+using namespace std;
+class student{
+    public:
+    int rno;
+    string name;
+    float marks;
+    void input(){
+        cin>>rno;
+        cin>>name;
+        cin>>marks;
+    }
+    void output(){
+        cout<<rno<<endl;
+        cout<<name<<endl;
+        cout<<marks<<endl;
+    }
+
+};
+int main(){
+    student s;
+    s.input();
+    s.output();
+    return 0;
+
+
+
+}
